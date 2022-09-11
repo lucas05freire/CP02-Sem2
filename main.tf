faa39e7458ed40c8eca67ae98fdeeef15872fb24
lucas05freire <lucasfreire.tec@gmail.com>
2022-09-11 20:07:50 -0300

    Uploading main.tf, index.html and error.html

---
+++ main.tf
@@ -0,0 +1,73 @@
+# PROVIDER
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# REGION
+provider "aws" {
+    region                  = "us-east-1"
+    shared_credentials_file = ".aws/credentials"
+}
+
+# BUCKET S3
+resource "aws_s3_bucket" "s3-95128" {
+  bucket = "s3-95128"
+}
+
+# STATIC SITE
+resource "aws_s3_bucket_website_configuration" "sites3" {
+  bucket = aws_s3_bucket.s3-95128.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# ACL S3
+resource "aws_s3_bucket_acl" "acls3" {
+  bucket = aws_s3_bucket.s3-95128.id
+  acl    = "public-read"
+}
+
+#S3 UPLOAD OBJECT
+resource "aws_s3_bucket_object" "error" {
+  key          = "error.html"
+  bucket       = aws_s3_bucket.s3-95128.id
+  source       = "error.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "index" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.s3-95128.id
+  source       = "index.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+# POLICY S3
+resource "aws_s3_bucket_policy" "policys3" {
+  bucket = aws_s3_bucket.s3-95128.id
+
+  policy      = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::s3-95128/*",
+      }
+    ]
+	})
+}
